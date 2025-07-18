bc822afed824129c2bfc43fcde3dadbee4b7c035
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-07-18 17:55:09 +0530

    Add CORS support

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("/", (req, res) => {
