72dd380c701d248c7d8cf3ea7cc731c66dd1c77d
Yashaswini K P <yashaswinikpyashu@gmail.com>
2026-08-06 16:01:28 +0530

    refactor: use backend streak data for HOT_STREAK badge evaluation (#494)

--- frontend/js/user/badges.js
+++ frontend/js/user/badges.js
@@ -31,45 +31,12 @@ export async function loadBadges(data) {
     const ranks = data.leaderboardRanks || {};
     const earnedSet = new Set();
 
+    if (data.streak && data.streak.current >= 7) {
+      earnedSet.add("HOT_STREAK");
+    }
+
     if (history.length >= 8) {
       const recent = history.slice(-8, -1);
-      let streak = 0;
-      let isValid = true;
-
-      for (let j = 1; j < recent.length; j++) {
-        const todayTotals = recent[j].easy + recent[j].medium + recent[j].hard;
-        const yesterdayTotals =
-          recent[j - 1].easy + recent[j - 1].medium + recent[j - 1].hard;
-
-        if (todayTotals - yesterdayTotals >= 1) {
-          streak++;
-        } else {
-          isValid = false;
-          break;
-        }
-      }
-
-      if (isValid) {
-        const currentDay = history[history.length - 1];
-        const previousDay = history[history.length - 2];
-        const solvedToday =
-          currentDay.easy +
-            currentDay.medium +
-            currentDay.hard -
-            (previousDay.easy + previousDay.medium + previousDay.hard) >=
-          1;
-
-        if (solvedToday) {
-          streak++;
-        }
-      } else {
-        streak = 0;
-      }
-
-      if (streak >= 7) {
-        earnedSet.add("HOT_STREAK");
-      }
-
       const first = recent[0];
       const last = recent[recent.length - 1];
       const solvedHard = last.hard - first.hard;
